fd486d1e21a12bcc47ee83e684ac592b022d69b6
Nanabena7 <91252519+Nanabena7@users.noreply.github.com>
2022-08-28 17:27:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+/**Author@ Nanabena**/
+namespace inventory
+{
+    static class Program
+    {
+     
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            loading welform = new loading();
+            welform.Show();
+            System.Threading.Thread.Sleep(3000);
+            welform.Hide();
+            Application.Run(new Form1());
+            
+
+       
+        }
+    }
+}
